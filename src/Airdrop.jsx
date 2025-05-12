9fcc0d7fe764563b1809421c05d37bad65b8df74
Sumeet Rana <sumeetsinh28@gmail.com>
2025-05-12 21:34:17 +0400

    feat: getting wallet public key

--- src/Airdrop.jsx
+++ src/Airdrop.jsx
@@ -1,7 +1,10 @@
+import { useWallet } from '@solana/wallet-adapter-react';
 import React from 'react';
 
 const Airdrop = () => {
+    const wallet = useWallet()
     return <div>
+        {wallet.publicKey && `Hey Mr ${wallet.publicKey.toString()}`}
        <input type="text" placeholder='Amout' />
        <button>Send Airdrop</button>
     </div>
